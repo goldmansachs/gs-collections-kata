42d48fc4bb4d75a202300ec0249966150f923b97
Craig P. Motlin <craig.motlin@gs.com>
2012-08-31 20:23:12 +0000

    [GSCKATA-13] Use array copying in Company.addSupplier().
    git-svn-id: svn+ssh://gsckata.svn.services.gs.com/svnroot/gsckata/branches/solutions@37 66aded80-052e-4344-91a8-38ab96726eea

--- src/main/java/com/gs/collections/kata/Company.java
+++ src/main/java/com/gs/collections/kata/Company.java
@@ -16,12 +16,9 @@
 
 package com.gs.collections.kata;
 
-import com.gs.collections.api.block.procedure.ObjectIntProcedure;
 import com.gs.collections.api.list.MutableList;
 import com.gs.collections.impl.block.factory.Predicates;
 import com.gs.collections.impl.list.mutable.FastList;
-import com.gs.collections.impl.utility.ArrayIterate;
-import com.gs.collections.impl.utility.Iterate;
 
 /**
  * A company has a {@link MutableList} of {@link Customer}s.  It has an array of {@link Supplier}s, and a name.
@@ -68,19 +65,10 @@ public class Company
     {
         // need to replace the current array of suppliers with another, larger array
         // Of course, normally one would not use an array.
+
         final Supplier[] currentSuppliers = this.suppliers;
         this.suppliers = new Supplier[currentSuppliers.length + 1];
-        ArrayIterate.forEachWithIndex(
-                currentSuppliers,
-                new ObjectIntProcedure<Supplier>()
-                {
-                    @Override
-                    public void value(Supplier eachSupplier, int index)
-                    {
-                        Company.this.suppliers[index] = currentSuppliers[index];
-                    }
-                }
-        );
+        System.arraycopy(currentSuppliers, 0, this.suppliers, 0, currentSuppliers.length);
         this.suppliers[this.suppliers.length - 1] = supplier;
     }
 
